2fb280a18c3696005d7140d1df2e21e72d82bf43
Rostislav Listerenko <r.listerenko@gmail.com>
2015-02-25 09:01:18 +0300

    Прикрутил SQLite #PT-6 fixed

---
+++ src/PicTureen/Database/ImagesDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.SQLite;
+
+namespace Database
+{
+    public class Tag
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class ImagesDbContext: DbContext
+    {
+        public DbSet<Tag> Tags { get; set; }  
+        public ImagesDbContext(IDbHelper dbHelper):base(new SQLiteConnection(dbHelper.ConnectionString), true)
+        {
+            
+        }
+    }
+
+    public interface IDbHelper
+    {
+        string ConnectionString { get; }
+    }
+}
---
+++ src/PicTureen/test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SQLite;
+using Database;
+
+namespace test
+{
+    class Program
+    {
+
+        static void CreateTables()
+        {
+            using (var conn = new SQLiteConnection(new Helper().ConnectionString))
+            {
+                conn.Open();
+                var tagTable = @"create table tags (Id bigint primary key, Name text)";
+                new SQLiteCommand(tagTable, conn).ExecuteNonQuery();
+            }
+        }
+
+        class Helper : IDbHelper
+        {
+            public string ConnectionString { get { return "DataSource = test.db; version = 3"; } }
+        }
+        static void Main(string[] args)
+        {
+            CreateTables();
+            using (var db = new ImagesDbContext(new Helper()))
+            {
+                foreach (var tag in db.Tags)
+                {
+                    Console.WriteLine(tag.Name);
+                }
+                Console.WriteLine("It works!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
