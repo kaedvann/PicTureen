7a333a33a910e7736ee066b17e9237216bce952e
Rostislav Listerenko <r.listerenko@gmail.com>
2015-03-25 11:35:03 +0300

    расширил тест для канера файлов #PT-7

--- src/PicTureen/MainProjectTests/FileScannerTests.cs
+++ src/PicTureen/MainProjectTests/FileScannerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using NUnit.Framework;
 using PicTureen.Services;
 
@@ -12,8 +13,29 @@ namespace MainProjectTests
         {
             var currentDirectory = TestContext.CurrentContext.TestDirectory;
             var scanner = new FileScanner();
-            var result = scanner.ScanDirectory(currentDirectory).Result;
+            var task = scanner.ScanDirectory(currentDirectory);
+            task.Wait();
+            var result = task.Result; 
             Assert.AreEqual(0, result.Count());
+            SampleFile(currentDirectory, "test1", ".jpg");
+            SampleFile(currentDirectory, "test2", ".png");
+            SampleFile(currentDirectory, "test3", ".bmp");
+            task = scanner.ScanDirectory(currentDirectory);
+            task.Wait();
+            result = task.Result;
+            Assert.AreEqual(3, result.Count());
+            SampleFile(currentDirectory, "test10", ".jpsdg");
+            SampleFile(currentDirectory, "test20", ".pngfg");
+            SampleFile(currentDirectory, "test30", ".bmdfp");
+            task = scanner.ScanDirectory(currentDirectory);
+            task.Wait();
+            result = task.Result;
+            Assert.AreEqual(3, result.Count());
+        }
+
+        void SampleFile(string directory, string name, string extension)
+        {
+            File.Create(Path.ChangeExtension(Path.Combine(directory, name), extension)).Dispose();
         }
     }
 }
