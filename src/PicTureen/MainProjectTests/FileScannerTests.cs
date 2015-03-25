417e31ce1b9de11582aa79b29a52a89e2bfe7980
Rostislav Listerenko <r.listerenko@gmail.com>
2015-03-25 10:54:37 +0300

    Создал класс, который будет возвращать имеющиеся файлы при старте и уведомлять о изменениях в рабочей директории #PT-7

---
+++ src/PicTureen/MainProjectTests/FileScannerTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using PicTureen.Services;
+
+namespace MainProjectTests
+{
+    [TestFixture]
+    public class FileScannerTests
+    {
+        [Test]
+        public void TestScanning()
+        {
+            var currentDirectory = TestContext.CurrentContext.TestDirectory;
+            var scanner = new FileScanner();
+            var result = scanner.ScanDirectory(currentDirectory).Result;
+            Assert.AreEqual(0, result.Count());
+        }
+    }
+}
---
+++ src/PicTureen/PicTureen/Services/IFileScanner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PicTureen.Services
+{
+    public interface IFileScanner
+    {
+        Task<IEnumerable<string>> ScanDirectory(string directory);
+    }
+
+    public class FileScanner : IFileScanner
+    {
+        private readonly string[] _supportedExtensions = {".png", ".jpg", ".bmp"};
+
+        public Task<IEnumerable<string>> ScanDirectory(string directory)
+        {
+            return Task<IEnumerable<string>>.Factory.StartNew(() =>Directory.EnumerateFiles(directory, ".", SearchOption.AllDirectories).Where(s => _supportedExtensions.Contains(Path.GetExtension(s), StringComparer.OrdinalIgnoreCase)));
+        }
+    }
+}
