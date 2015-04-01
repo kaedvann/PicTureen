b1d794edd3832f0851063e5bfadc620a3465b064
Valerie Karabatyrova <looneyplatypus@gmail.com>
2015-04-01 20:37:27 +0300

    Добавлены заигнореные бинарники в решарпер #PT-10

---
+++ tools/Check-Style.ps1
@@ -0,0 +1,5 @@
+﻿param($cmd = 'E:\Coding\C#\PicTureen\tools\ResharperTools\inspectcode.exe',
+      $output = 'E:\result.xml',
+      $solution = 'E:\Coding\C#\PicTureen\src\PicTureen\PicTureen.sln')
+
+&$cmd $solution $output
