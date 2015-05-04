8b8478628f473f38241cf28ab064655e45d5738e
Rostislav Listerenko <r.listerenko@gmail.com>
2015-05-04 21:34:54 +0300

    Ugly picture moving

--- src/PicTureen/PicTureen/Views/FullImageView.xaml.cs
+++ src/PicTureen/PicTureen/Views/FullImageView.xaml.cs
@@ -16,33 +16,6 @@ namespace PicTureen.Views
             InitializeComponent();
         }
 
-        private bool isMoving;
-        private double dX, dY;
-        private Point _startPoint;
-        private void DispImage_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            _startPoint = e.GetPosition((IInputElement) sender);
-            isMoving = true;
-        }
 
-        private void DispImage_OnMouseMove(object sender, MouseEventArgs e)
-        {
-            if (isMoving && e.LeftButton == MouseButtonState.Pressed)
-            {
-                Point newPoint = e.MouseDevice.GetPosition((IInputElement) sender);
-                if (Math.Abs(newPoint.X - _startPoint.X) >= 1 || Math.Abs(newPoint.Y - _startPoint.Y) >= 1)
-                {
-                    TranslateTransform.X = newPoint.X - _startPoint.X;
-                    TranslateTransform.Y = newPoint.Y - _startPoint.Y;
-                    _startPoint = newPoint;
-                }
-            }
-
-        }
-
-        private void DispImage_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            isMoving = false;
-        }
     }
 }
